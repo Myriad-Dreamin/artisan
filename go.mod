f4b8fda64fc3d704d5f6ac121002edca3adb8b24
Myriad-Dreamin <camiyoru@gmail.com>
2020-07-09 19:03:04 +0800

    update inner-helper_test

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Myriad-Dreamin/artisan
 
 go 1.13
 
-require github.com/Myriad-Dreamin/minimum-lib v0.0.0-20191222020721-aa5c7417baf3
+require (
+	github.com/Myriad-Dreamin/minimum-lib v0.0.0-20191222020721-aa5c7417baf3
+	github.com/stretchr/testify v1.4.0
+)
--- inner-helper_test.go
+++ inner-helper_test.go
@@ -1,12 +1,14 @@
 package artisan
 
-import "fmt"
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestInnerFromBigCamelToSnake(t *testing.T) {
-	fmt.Println(fromBigCamelToSnake("test"))
-	fmt.Println(fromBigCamelToSnake("Test"))
-	fmt.Println(fromBigCamelToSnake("TestID"))
-	fmt.Println(fromBigCamelToSnake("ABC"))
-	fmt.Println(fromBigCamelToSnake("QwQ"))
+	assert.Equal(t, fromBigCamelToSnake("test"), "test")
+	assert.Equal(t, fromBigCamelToSnake("Test"), "test")
+	assert.Equal(t, fromBigCamelToSnake("TestID"), "test_id")
+	assert.Equal(t, fromBigCamelToSnake("ABC"), "abc")
+	assert.Equal(t, fromBigCamelToSnake("QwQ"), "qw_q")
 }
